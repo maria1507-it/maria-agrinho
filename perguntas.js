8ac031df03638fe272e75ad7f0d1a7e10edd65d9
maria1507-it <maria.viecelli@escola.pr.gov.br>
2026-06-12 10:49:14 -0300

    grii

---
+++ perguntas.js
@@ -0,0 +1,70 @@
+criarcartão(
+    'agrinho',
+    'oque significa "agro forte"
+    'significa um setor agricola desenvolvido, produto importante para a economia e para alimentação da população'
+)
+
+criarcartão(
+    'agrinho'
+    'por que o agro é importante para o futuro sustentável'
+    'porque produz alimentos, gera empregos e pode utilizar práticas que preservem o meio ambiente'
+)
+
+criarcartão(
+    'o que é sustentabilidade no campo'
+    'e produzir alimentos sem causar grandes danos a'natureza, presevando recursos para as futuras gerações'
+    
+)
+
+criarcartão(
+    'como a tecnologia ajuda o agro sustentável'
+    'a tecnologia ajuda a economizar água, reduzir desperdícios e aumentar a produtividade de forma mais consciente'
+
+)
+
+criarcartão(
+    ' o que é equilibrio entre produção e meio ambiente'
+    'é produzir o necessario sem destruir floresta, rios, solos, e animais'
+
+)
+
+criarcartão(
+    'por que preservar o solo é importante na agricultuira'
+    'porque o solo saudável melhora a produção e evita erosões e perdas de nutrientes'
+
+)
+
+criarcartão(
+    'quais são os benefícios de um agro sustentável para a sociedade'
+    'geração de empregos, produção de alimentos de qualidade, preservação ambiental e desenvolvimento economico'
+
+)
+
+ criarcartão(
+    'como os produtos rurais podem ajudar a proteger o meio ambiente'
+    'utilizando práticas sustentáveis, como conservação do solo, uso racional da água e preservação das áreas de vegetação nativa'
+
+ )
+
+ criarcartão(
+    'o que pode acontecer se os recursos naturais forem utilizados de forma inadequada no campo'
+    'pode ocorrer degradação do solo, poluição da água e redução da produtividade agrícola'
+
+ )
+
+ criarcartão(
+    'quais práticas ajudam a tornar o agro mais sustentável'
+    'rotação de culturas, preservação  das matas, uso eficiente da água e redução do desperdício'
+
+ )
+
+ criarcartão(
+    'por que é importante conciliar produção agrícola e preservação ambiental'
+    'porque ambos são essenciais para garantir alimentos, qualidade de vida e equilíbrio ecológico'
+
+ )
+
+ criarcartão(
+    'como o consumidor pode contribuir para um futuro mais sustentável no agro'
+    'consumindo de forma consciente, evitando desperdícios e valorizando produtos produzidos de maneira sustentável'
+ )
